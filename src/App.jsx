fc0e66ce32c016a4a5775a84d78f3d6d492255a1
masudrana-1 <masudrana58855@gmail.com>
2023-12-04 11:28:02 +0600

    added icon and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import logo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import message from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import pro from './assets/rocket.svg';
 
 // import './App.css'
 
@@ -9,20 +14,22 @@ const App = () => {
             <div className="sideBar">
                 <div className="upperSide">
                     <div className="upperSideTop">
-                        <img src={logo} alt="" className="logo" />
+                        <img src={logo} alt="logo" className="logo" />
                         <span className="brand">ChatGPT</span>
                     </div>
                     <button className="miBtn">
-                        <img src="" alt="" className="addBtn" />
+                        <img src={addBtn} alt="" className="addBtn" />
                         New Chat
                     </button>
                     <div className="upperSideBottom">
-                        <button className="query"><img src="" alt="" />What is programming?</button>
-                        <button className="query"><img src="" alt="" />What is programming?</button>
+                        <button className="query"><img src={message} alt="" />What is programming?</button>
+                        <button className="query"><img src={message} alt="" />How to use an API?</button>
                     </div>
                 </div>
                 <div className="lowerSide">
-
+                    <div className="listItems"><img src={home} alt="" className="listItemImg" />Home</div>
+                    <div className="listItems"><img src={saved} alt="" className="listItemImg" />Saved</div>
+                    <div className="listItems"><img src={pro} alt="" className="listItemImg" />Upgrade to pro</div>
                 </div>
             </div>
             <div className="main">
